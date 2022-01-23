0effa53bd1c14ec9e92a0d9466eddc1951daabbb
Oleg Filippov <5157175@bk.ru>
2022-01-23 20:20:57 +0300

    Initial_commit_@cvsfile

---
+++ src/main/java/ru/netology/BonusService.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+public class BonusService {
+    // подготавливаем данные:
+    long amount = 1000;
+    boolean registered = true;
+    long expected = 30;
+
+    public long calculate(long amount, boolean registered) {
+        int percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100 / 100;
+        long limit = 500;
+        if (bonus > limit) {
+            bonus = limit;
+        }
+        return bonus;
+    }
+}
+
---
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+ /*   @Test
+    void calculate() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+        System.out.println(actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Test1, 1000_60, true, 30"})
+    void calculate(String testName, long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    } */
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void csvFileTest(long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+    }
+}
